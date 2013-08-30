c878881f18bd1436374b14c1d11deaa1e028bfe0
Jon Burgess <jkburges@gmail.com>
2013-08-30 11:07:20 +1000

    Basic app, with Guard set up for auto browser reload.

---
+++ custom/src/Viewport.js
@@ -0,0 +1,9 @@
+Ext.onReady(function() {
+
+    new Ext.Viewport({
+        items: [
+            {
+                html: '<h1 class="x-panel-header">Page Title</h1>'
+            }
+        ]});
+});
